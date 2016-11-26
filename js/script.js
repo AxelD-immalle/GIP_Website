ac7dedf3524bcf9feaf65fe8e8dc0fb7f0265f96
Axel Dries <axel.dries@immalle.eu>
2016-11-26 16:24:25 +0100

    Stage boxes

--- js/script.js
+++ js/script.js
@@ -1,15 +1,7 @@
 $('.button-collapse').sideNav({
-    menuWidth: 300, // Default is 240
+    menuWidth: 240, // Default is 240
     edge: 'left', // Choose the horizontal origin
     closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
     draggable: true // Choose whether you can drag to open on touch screens
   }
 );
-
-function submitFunction() {
-    alert("Deze button verstuurd nog geen contact formulier. Moest ik deze pagina laten staan, zal ik dit wel in orde brengen.");
-}
-
-var sumbitButton = document.getElementById("sendButton");
-
-sumbitButton.addEventListener("click", submitFunction);
